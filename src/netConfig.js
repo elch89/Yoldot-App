164c8b74f466bed3aff1c32952f4d0689035e495
elch89 <33932581+elch89@users.noreply.github.com>
2022-05-04 15:33:32 +0300

    Delete src directory

--- src/netConfig.js
+++
@@ -1,6 +0,0 @@
-/**
- * Configure url globaly
- */
-export default connection={
-    URL:'http://45.9.190.147/',
-};
--- src/styles/colors.js
+++
@@ -1,9 +0,0 @@
-
-export default myColors = {
-    lightBlue:'#87ccee',
-    darkBlue:'#2b426f',
-    red:'#a04154',
-    gold:'#dba55a',
-    black:'#fff',
-    white:'#000'
-}
